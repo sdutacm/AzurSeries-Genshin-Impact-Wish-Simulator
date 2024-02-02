09f7adc149bad0d105f730b5f46499cfbdd8c875
bLue <tbdblue@gmail.com>
2024-02-02 21:17:51 +0800

    chore: use cf adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,16 @@
 import path from 'path';
-import adapter from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		appDir: 'internal',
-		adapter: adapter(),
+		// adapter: adapter(),
+		adapter: adapter({
+			// See https://kit.svelte.dev/docs/adapter-cloudflare
+		}),
 		alias: {
 			$post: path.resolve('./src/post')
 		}
